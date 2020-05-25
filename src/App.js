d95f5f04d1e747047dadf78ba5086949dbba2850
anjelra <puppy912000@naver.com>
2020-05-25 17:54:24 +0900

    header component 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // 1. function 형식으로 사용할 수 있다.
@@ -27,30 +26,37 @@ import './App.css';
 // }
 
 // class 형식
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         Hello, React!
+//       </div>
+//     )
+//   }
+// }
+
+class Subject extends Component {
+  // 자바스크립트 최신 스펙.. class 안에 선언된 function은 function 생략. 따라서 render()는 함수임.
+  // 주의!!!! component를 만들 때는 반드시 하나의 최상위 태그로 시작해야함.
+  // 여기서는 header가 최상위 태그임..
+  render() {
+    return (
+      <header>
+            <h1>WEB</h1>
+            world wide web!
+        </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!
+        <Subject></Subject>
       </div>
-    //   <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    )
-  }
+    );
+  };
 }
-
 export default App;
