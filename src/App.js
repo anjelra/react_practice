db5adabd9383915f6b37ab5823437d49d1ad588b
anjelra <puppy912000@naver.com>
2020-05-25 17:59:51 +0900

    component는 정리정돈의 의미도 가진다

--- src/App.js
+++ src/App.js
@@ -50,11 +50,39 @@ class Subject extends Component {
   }
 }
 
+// table of content(TOC) -> 목차
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">Javascript</a></li>
+          </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   };
