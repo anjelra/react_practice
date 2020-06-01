4865474ecf53e5ddfdc7a8d58ee242d259209054
anjelra <puppy912000@naver.com>
2020-06-01 14:47:19 +0900

    render 함수 전에 호출되는 shouldComponentUpdate 함수에 대해 알아보자

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,6 +3,21 @@ import React, { Component } from 'react';
 
 // table of content(TOC) -> 목차
 class TOC extends Component {
+    // shouldComponentUpdate 함수는 render() 함수 이전에 호출된다.
+    // shouldComponentUpdate 함수의 return 값이 false면 render() 함수는 호출되지 않는다.
+    // shouldComponentUpdate 함수의 return 값이 true면 render() 함수가 호출된다.
+
+    // 그런데 만약에, App.js에서 state.contents의 state값을 concat으로 하지 않고 push로 한다면
+    // newProps의 값과 this.props.data의 값이 반드시 같으므로 push는 사용하지 않는게 좋다.
+
+    // shouldComponentUpdate 함수가 필요없는 경우에는 그냥 push 를 사용해도 상관이 없다.
+    shouldComponentUpdate(newProps, newState) {
+        if (this.props.data === newProps.data) {
+            return false;
+        }
+        return true;
+    }
+
     render() {
         var lists = [];
         var data = this.props.data;
